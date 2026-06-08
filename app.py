4a0b7d42c5b3cb4033f16c474bbcf6718632758b
hirushgogoi-ship-it <hirushgogoi@gmail.com>
2026-06-08 09:48:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ if st.button("Analyze"):
     if api_key and input_data:
         try:
             genai.configure(api_key=api_key)
-            model = genai.GenerativeModel('gemini-1.5-flash')
+            model = genai.GenerativeModel('gemini-1.5-flash-latest')
+            
             response = model.generate_content(input_data)
             st.write(response.text)
         except Exception as e:
